ecc27fb796ef4552792207c5955d26984f63deba
Itihell Mejia <itihell.mejia@gmail.com>
2025-04-25 20:35:04 -0600

    VAlidar opcion nula desde una funcion

--- utils.py
+++ utils.py
@@ -26,24 +26,35 @@ class Utils:
         for opcion in OpcionMenu:
             print(f"###### {opcion.value}. {opcion.name}")
         print("###############################")
-        
 
-    def validar_opcion_menu(self):
-        print(Fore.YELLOW + Style.BRIGHT)
-        opcion=input("\nSeleccione una opción: ")
-        # validar si opcion esta vacia
+    def es_opcion_valida(self, opcion:str):
         if not opcion:
             self.limpiar_pantalla()
             print(Fore.RED + Style.BRIGHT)
             print("No tecleo una opción del menú. Intente de nuevo.")
             self.pintar_pausa()
+
+        if len(opcion)==0:
+            self.limpiar_pantalla()
+            print(Fore.RED + Style.BRIGHT)
+            print("No selecciono una opción. Intente de nuevo.")
+            self.pintar_pausa()
+        
         # validar si option no es un número
         if not opcion.isdigit():
             self.limpiar_pantalla()
             print(Fore.RED + Style.BRIGHT)
             print("Debe seleccionar una opción válida. Intente de nuevo.")
             self.pintar_pausa()
-        opcion=int(opcion)
+
+        return opcion
+
+
+    def validar_opcion_menu(self):
+        print(Fore.YELLOW + Style.BRIGHT)
+        opcion=input("\nSeleccione una opción: ")
+        # validar si opcion esta vacia        
+        opcion=int(self.es_opcion_valida(opcion))
         self.validar_opcion(opcion)
     
     def validar_opcion(self,opcion:int):
